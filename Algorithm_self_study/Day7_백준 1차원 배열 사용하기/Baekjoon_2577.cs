f664781114377adeb20d3a2520d4019d160246f0
microsong <jhn06046@hotmail.com>
2019-02-18 16:04:07 +0900

    Day7 _ 1차원 배열 사용해보기(숫자의 개수, OX퀴즈)

---
+++ Algorithm_self_study/Day7_백준 1차원 배열 사용하기/Baekjoon_2577.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day7_백준_1차원_배열_사용하기
+{
+    class Baekjoon_2577
+    {
+        static void Main(string[] args)
+        {
+            int multiple = 0;
+            int[] arr = new int[10];    //0~9
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            multiple = n1 * n2 * n3;
+            do
+            {
+                arr[multiple % 10]++;
+                multiple /= 10;
+            } while (multiple != 0);
+            foreach(int result in arr)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Algorithm_self_study/Day7_백준 1차원 배열 사용하기/Baekjoon_8958.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day7_백준_1차원_배열_사용하기
+{
+    class Baekjoon_8958
+    {
+        static void Main(string[] args)
+        {
+            // 개수 입력
+            int cnt = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < cnt; i++)
+            {
+                // 문자열 입력
+                string word = Console.ReadLine();
+                // 반복문으로 내부 문자 비교
+                int sum = 0;    // 결과값
+                int score = 1;  //기본 값 
+                for(int j=0; j<word.Length; j++)
+                {
+                    if(word[j] == 'O')
+                    {
+                        sum += score;
+                        score++;
+                    }
+                    else
+                    {
+                        score = 1;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
