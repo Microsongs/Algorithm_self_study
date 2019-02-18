e2150afe1381a5fad04cabfafaf26c29fa380cf4
microsong <jhn06046@hotmail.com>
2019-02-18 15:41:31 +0900

    Day7 - 문제 1152

---
+++ Algorithm_self_study/Day7_백준 1차원 배열 사용하기/Baekjoon_1152.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day7_백준_1차원_배열_사용하기
+{
+    class Baekjoon_1152
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            string str = Console.ReadLine();
+            string[] tmp = str.Split();
+            size = tmp.Length;
+            if(str.IndexOf(" ") == 0)
+            {
+                size--;
+            }
+            if(str.LastIndexOf(" ") == str.Length-1)
+            {
+                size--;
+            }
+            Console.WriteLine(size);
+        }
+    }
+}
