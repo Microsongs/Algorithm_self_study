b12cbf0cb134163694bd8d7a5552994ea5f21625
microsong <jhn06046@hotmail.com>
2019-02-19 13:26:40 +0900

    day8_백준 1차원 배열 사용하기 完

---
+++ Algorithm_self_study/Day8_백준 1차원 배열 사용하기2/Baekjoon_10039.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day8_백준_1차원_배열_사용하기2
+{
+    class Baekjoon_10039
+    {
+        static void Main(string[] args)
+        {
+            int[] stu = new int[5];
+            int sum = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                stu[i] = int.Parse(Console.ReadLine());
+                if(stu[i] < 40)
+                {
+                    stu[i] = 40;
+                }
+                sum += stu[i];
+            }
+            Console.WriteLine(sum / 5);
+        }
+    }
+}
---
+++ Algorithm_self_study/Day8_백준 1차원 배열 사용하기2/Baekjoon_2920.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day8_백준_1차원_배열_사용하기2
+{
+    class Baekjoon_2920
+    {
+        static void check(string[] data, int op)
+        {
+            for(int i = 2; i < data.Length; i++)
+            {
+                if(int.Parse(data[i]) != int.Parse(data[0]) + (i * op))
+                {
+                    Console.WriteLine("mixed");
+                    return;
+                }
+            }
+            if (op == -1)
+            {
+                Console.WriteLine("descending");
+            }
+            else {
+                Console.WriteLine("ascending");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] data = input.Split();
+            int tmp = int.Parse(data[0]);
+            if(int.Parse(data[1]) == tmp + 1)
+            {
+                check(data, 1);
+            }
+            else if(int.Parse(data[1]) == tmp - 1)
+            {
+                check(data, -1);
+            }
+            else
+            {
+                Console.WriteLine("mixed");
+            }
+        }
+    }
+}
