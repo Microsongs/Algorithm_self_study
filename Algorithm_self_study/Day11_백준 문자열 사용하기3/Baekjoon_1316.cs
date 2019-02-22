795dd36011c465947ff20ec79a8f7b3b5c7d91e8
microsong <jhn06046@hotmail.com>
2019-02-22 16:34:52 +0900

    Day11_백준 문자열 사용하기 完

---
+++ Algorithm_self_study/Day11_백준 문자열 사용하기3/Baekjoon_1316.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day11_백준_문자열_사용하기3
+{
+    class Baekjoon_1316
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int groupChecker = 0;
+            bool state = false;
+            for(int i=0; i<number; i++)
+            {
+                int[] alphabet = new int[26];
+                string word = Console.ReadLine();   //문자열 입력
+                for(int j=0; j<word.Length; j++)
+                {
+                    if(alphabet[word[j] - 'a'] == 0)    //처음 나오는 단어인가?
+                    {
+                        alphabet[word[j] - 'a'] = 1;
+                    }
+                    else if(alphabet[word[j] - 'a'] == 1 && (word[j-1] != word[j])) //앞뒤로 다른 단어이면서 이미 들어가있는 단어일 경우
+                    {
+                        alphabet[word[j] - 'a'] = 2;
+                    }
+                }
+                //개수 체크
+                foreach(int checker in alphabet)
+                {
+                    if(checker == 2)
+                    {
+                        state = false;
+                        break;
+                    }
+                    if(checker == 1)
+                    {
+                        state = true;
+                    }
+                }
+                groupChecker = (state == true) ? groupChecker+1 : groupChecker;
+            }
+            Console.Write(groupChecker);
+        }
+    }
+}
---
+++ Algorithm_self_study/Day11_백준 문자열 사용하기3/Baekjoon_2941.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day11_백준_문자열_사용하기3
+{
+    class Baekjoon_2941
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int len = 0;
+            for(int i=0; i<word.Length; i++)
+            {
+                //알파벳이 맞는지 확인
+                if(word.Length-1 > i)
+                {
+                    switch (word[i])
+                    {
+                        case 'c':
+                            if (word[i+1] == '=' || word[i+1] == '-')   //c= 와 c-
+                                i++;
+                            break;
+                        case 'd':
+                            if (word[i + 1] == '-') //d-
+                                i++;
+                            else if (word.Length+2 == i && word[i + 1] == 'z' || word[i + 2] == '=')  //dz=
+                                i += 2;
+                            break;
+                        case 'l':
+                            if (word[i + 1] == 'j')
+                                i++;
+                            break;
+                        case 'n':
+                            if (word[i + 1] == 'j')
+                                i++;
+                            break;
+                        case 's':
+                            if (word[i + 1] == '=')
+                                i++;
+                            break;
+                        case 'z':
+                            if (word[i + 1] == '=')
+                                i++;
+                            break;
+                    }
+                }
+                len++;
+            }
+            Console.WriteLine(len);
+        }
+    }
+}
