b9df98c1740361fc9abd9a39805818a874d78e7b
microsong <jhn06046@hotmail.com>
2019-03-12 16:15:29 +0900

    Day21 - 백준 소트인사이드 Radix SOrt는 미완

---
+++ Algorithm_self_study/Day15,19~_정렬해보기/Baekjoon_10989.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day15_19__정렬해보기
+{
+    class Baekjoon_10989
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] arr = new int[10001];
+            for(int i=0; i<count; i++)
+            {
+                arr[int.Parse(Console.ReadLine())]++;
+            }
+            for(int i=0; i<arr.Length; i++)
+            {
+                if(arr[i] > 0)
+                {
+                    for(int arri = 0; arri < arr[i]; arri++)
+                    {
+                        Console.Write(i + "\n");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithm_self_study/Day15,19~_정렬해보기/Baekjoon_1427.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day15_19__정렬해보기
+{
+    class Baekjoon_1427
+    {
+        static void Main(string[] args)
+        {
+            int data = int.Parse(Console.ReadLine());
+            List<int> myData = new List<int>();
+            int len = 0;
+            for(; data!=0; data /= 10, len++)
+            {
+                myData.Add(data % 10);  //데이터에 int를 넣는다.
+            }
+            for (int i = 0; i < len-1; i++)
+            {
+                for (int j = 0; j < len-1-i; j++)
+                {
+                    if (myData[j] < myData[j + 1])
+                    {
+                        int temp = myData[j];
+                        myData[j] = myData[j + 1];
+                        myData[j + 1] = temp;
+                    }
+                }
+            }
+            foreach(int arr in myData)
+            {
+                Console.Write(arr);
+            }
+        }
+    }
+}
