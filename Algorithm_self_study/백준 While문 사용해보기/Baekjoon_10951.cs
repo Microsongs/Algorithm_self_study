c183f5692f3f398c042782a21ab17f09be4e23ef
microsong <jhn06046@hotmail.com>
2019-07-04 08:34:46 +0900

    백준 While문 사용해보기 10951,10952

---
+++ Algorithm_self_study/백준 While문 사용해보기/Baekjoon_10951.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.백준_While문_사용해보기
+{
+    class Baekjoon_10951
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string[] temp;
+
+            while((data = Console.ReadLine()) != null)
+            {
+                temp = data.Split();
+                Console.WriteLine(int.Parse(temp[0]) + int.Parse(temp[1]));
+            }
+        }
+    }
+}
---
+++ Algorithm_self_study/백준 While문 사용해보기/Baekjoon_10952.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.백준_While문_사용해보기
+{
+    class Baekjoon_10952
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string[] temp;
+            int a, b;
+            while (true)
+            {
+                input = Console.ReadLine();
+                temp = input.Split();
+                a = int.Parse(temp[0]);
+                b = int.Parse(temp[1]);
+                if ((a == 0) && (b == 0))
+                    break;
+                else
+                    Console.WriteLine(a+b);
+            }
+        }
+    }
+}
