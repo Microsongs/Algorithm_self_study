2c1ff3f581c47aa9a74503b6ad25ac53ab229eba
microsong <jhn06046@hotmail.com>
2019-07-03 09:02:47 +0900

    백준 단계별로 풀어보기 for문부분 추가문제

---
+++ Algorithm_self_study/Day4_백준 for문 사용해보기/Baekjoon_10950.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_self_study.Day4_백준_for문_사용해보기
+{
+    class Baekjoon_10950
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());    //길이 입력
+            int[] arr = new int[len];
+            string input;
+            string[] temp;
+
+            //숫자들 입력
+            for (int i=0; i<len; i++)
+            {
+                input = Console.ReadLine();
+                temp = input.Split();
+                arr[i] = int.Parse(temp[0]) + int.Parse(temp[1]);
+            }
+
+            foreach(int data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
